653ef63393004ac7d01be821342020ebe585de86
Anshuman Gupta <gupta.anshuman20@gmail.com>
2022-02-14 14:10:47 +0530

    Extracted embeddings from team images

--- test_embeddings_creator.py
+++ test_embeddings_creator.py
@@ -3,14 +3,21 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from utils import datagen_to_np_array,saveEmbeddingFile
 import numpy as np
 import sys
+import os
 
-DATAPATH = "cropped_images_team1/train/"
-#model.summary()
+try:
+    DATAPATH = sys.argv[1]
+    EXTRACTOR_PATH = sys.argv[2]
+    EMBEDDINGS_FILENAME = sys.argv[3]
+except IndexError:
+    exit("Please pass commandline arguments. Run the script by passing <dataset_path> <extractor_path> <target_filename>")
+    
+EMBEDDINGS_BASE_PATH = "embeddings/"
 
 IMAGE_SIZE = (128, 128)
 COLOR_MODE = "grayscale"
 IMAGE_SHAPE = list(IMAGE_SIZE)+[3]
-NUM_CLASSES = 4
+
 
 # Initialize generator
 datagen = ImageDataGenerator(rescale=1./255,brightness_range=(1.0, 1.3))
@@ -29,12 +36,15 @@ print("Labels array shape: ", labels.shape)
 
 
 # Extract Embeddings
-embeddings_extractor = load_model("models/team_embeddings_extractor_v2", compile=False)
+embeddings_extractor = load_model(EXTRACTOR_PATH, compile=False)
 embeddings = embeddings_extractor(images).numpy()
 
 # Save embeddings
 print("Embedding successfully extracted. Shape:",embeddings.shape)
 embeddings_list = [{'embedding':list(embedding.astype('float64')), 'class_label':str(label)} for embedding, label in zip(embeddings, labels)]
-EMBEDDINGS_PATH = "embeddings/team_embeddings_v2.json"
-saveEmbeddingFile(EMBEDDINGS_PATH, embeddings_list)
-print("Embeddings save successfully in",EMBEDDINGS_PATH)
+
+if not os.path.exists(EMBEDDINGS_BASE_PATH):
+    os.makedirs(EMBEDDINGS_BASE_PATH)
+save_path = os.path.join(EMBEDDINGS_BASE_PATH, EMBEDDINGS_FILENAME+".json") 
+saveEmbeddingFile(save_path, embeddings_list)
+print("Embeddings save successfully in", save_path)
