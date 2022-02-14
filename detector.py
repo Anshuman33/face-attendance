3be114fdb8b9b7c5d3b2bc743d73c4837f4f70f1
Anshuman Gupta <gupta.anshuman20@gmail.com>
2022-02-15 03:05:49 +0530

    Added threaded video stream in detector

--- detector.py
+++ detector.py
@@ -1,18 +1,26 @@
 import cv2
 from mtcnn import MTCNN
+from imutils.video import WebcamVideoStream
 import recognizer
 
 
 detector = MTCNN()
-#face_cascade = cv2.CascadeClassifier("L:/Anaconda3/envs/facedet/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+# face_cascade = cv2.CascadeClassifier("L:/Anaconda3/envs/facedet/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
 
-cap = cv2.VideoCapture(0)
+#SOURCE = "D:/CodingAndProjects/AI Project/VID_20220215_005004057.mp4"
+SOURCE = 0
+# SOURCE = "http://192.168.29.202:8080/video"
+
+# Starts a thread for capturing video frames from camera or video input
+cap = WebcamVideoStream(src=SOURCE).start()
 
 while True:
     # Read the frame
-    _, img = cap.read()
+    img = cap.read()
+    img = cv2.resize(img, (512,512))
     
     detections = detector.detect_faces(img)
+    
     # Detect the faces
     face_boxes = []
     for detection in detections:
@@ -26,14 +34,13 @@ while True:
         
         # Recognize the class label
         class_label = recognizer.recognizeClass(cropped_image,5)
-    
         
         # Draw the rectangle around the face
-        img = cv2.rectangle(img, (x, y), ((x+w), (y+h)), (255, 0, 0), 3)
-        cv2.putText(img, class_label,(x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        img = cv2.rectangle(img, (x, y), ((x+w), (y+h)), (255, 0, 0), 2)
+        cv2.putText(img, class_label,(x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (36,255,12), 2)
     
     # Display
-    cv2.imshow('Video', img)
+    cv2.imshow('Face Recognizer', img)
     
     # Stop if escape key is pressed
     k = cv2.waitKey(30) & 0xFF
@@ -41,4 +48,6 @@ while True:
         break
     
 # Release video capture object
-cap.release()
+#cap.release()
+cv2.destroyAllWindows()
+cap.stop()
