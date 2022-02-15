707d7a30c959672dcaac2989e638ff0847cd7c52
Anshuman Gupta <gupta.anshuman20@gmail.com>
2022-02-16 02:29:10 +0530

    Added threaded detector

--- recognizer.py
+++ recognizer.py
@@ -1,18 +1,16 @@
 from sklearn import neighbors
 from utils import convertToThreeChanneled, getEmbeddingsFromFile
 import numpy as np
-from collections import Counter
 from tensorflow.keras.models import load_model
 from PIL import Image, ImageOps
-from sklearn.metrics import euclidean_distances
 from sklearn.neighbors import KNeighborsClassifier
 
-test_embeddings, test_labels = getEmbeddingsFromFile('embeddings/team_embeddings_v3.json')
-knn = KNeighborsClassifier()
+test_embeddings, test_labels = getEmbeddingsFromFile('embeddings/team_embeddings_v5.json')
+knn = KNeighborsClassifier(weights="distance")
 knn.fit(test_embeddings, test_labels)
 class_index_lookup = {v:k for k,v in enumerate(knn.classes_)}
 
-MODEL_PATH = "embeddings_models/team_data_models/embeddingModel_mobilenet_siamese_96valAccV2"
+MODEL_PATH = "embeddings_models/team_data_models/embeddingModel_mobilenet_siamese_98Acc"
 feature_extractor = load_model(MODEL_PATH, compile=False)
 
 def preprocessImage(image_arr):
@@ -23,7 +21,7 @@ def preprocessImage(image_arr):
     image = convertToThreeChanneled(image)
     return np.expand_dims(image, axis=0)
 
-def recognizeClass(new_image, k=10):
+def recognizeClass(new_image, k=5):
     knn.n_neighbors = k
     preprocessed = preprocessImage(new_image)
     
@@ -31,6 +29,6 @@ def recognizeClass(new_image, k=10):
     embedding = feature_extractor(preprocessed)
     y = knn.predict(embedding)[0]
     score = knn.predict_proba(embedding)[0][class_index_lookup[y]]
-    return f"{y}[{score}]"   
+    return f"{y} [{score:.2f}]"   
     
     
--- threaded_detector.py
+++ threaded_detector.py
@@ -1,6 +1,6 @@
+from imutils.video import WebcamVideoStream
 import cv2
 from mtcnn import MTCNN
-from imutils.video import WebcamVideoStream
 import recognizer
 
 detector = MTCNN()
@@ -19,25 +19,18 @@ def detectFaceUsingMTCNN(image):
             face_boxes.append(detection["box"])
             
     return face_boxes
-    
 
+#SOURCE = "D:/CodingAndProjects/AI Project/VID_20220215_005004057.mp4"
+# SOURCE = 0
+SOURCE = "http://192.168.29.180:8080/video"
 
-# SOURCE = "D:/CodingAndProjects/AI Project/test_videos/anshuman.mp4"
-SOURCE = "D:/CodingAndProjects/AI Project/test_videos/monalisa.mp4"
-# SOURCE = "D:/CodingAndProjects/AI Project/test_videos/kirti.mp4"
-# SOURCE = "D:/CodingAndProjects/AI Project/test_videos/sattwik2.mp4"
-
-# Starts capturing video frames from camera or video input
-cap = cv2.VideoCapture(SOURCE)
+# Starts a thread for capturing video frames from camera or video input
+cap = WebcamVideoStream(src=SOURCE).start()
 
 while True:
     # Read the frame
-    _, img = cap.read()
-    if img is None:
-        break
-    
-    img = cv2.resize(img, (100,100))
-    
+    img = cap.read()
+    img = cv2.resize(img, (128,128))
     
     # Detect the faces
     face_boxes = detectFaceUsingCascadeClassifier(img)
@@ -51,11 +44,10 @@ while True:
         class_label = recognizer.recognizeClass(cropped_image,5)
         
         # Draw the rectangle around the face
-        img = cv2.rectangle(img, (x, y), ((x+w), (y+h)), (255, 0, 0), 1)
-        cv2.putText(img, class_label,(x-15, y+h+10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (36,255,12), 1)
+        img = cv2.rectangle(img, (x, y), ((x+w), (y+h)), (255, 0, 0), 2)
+        cv2.putText(img, class_label,(x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (36,255,12), 2)
     
     # Display
-    img = cv2.resize(img, (256, 256))
     cv2.imshow('Face Recognizer', img)
     
     # Stop if escape key is pressed
@@ -65,4 +57,4 @@ while True:
     
 # Release video capture object
 cv2.destroyAllWindows()
-cap.release()
+cap.stop()
